f6b8f52b6860dc77eae49c539226b1b515f152b0
ratuz <raul@innku.com>
2010-10-11 16:46:16 -0500

    Added hit counter to articles and started the act as taggable on

--- article.rb
+++ article.rb
@@ -1,7 +1,16 @@
 class Article < ActiveRecord::Base
   
+  acts_as_taggable
+  
   belongs_to :category
   belongs_to :user
   validate :content,:description,:category,:user, :presence => true
+  
+  def increase_count!
+    self.hit_count += 1  
+    self.save!
+  end
+  
+  
  
 end
