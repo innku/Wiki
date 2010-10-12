40c83015c79f10a7845c3385832631df441f59d3
ratuz <raul@innku.com>
2010-10-12 11:11:03 -0500

    change the width and rows of textarea in articles and corrected the symbols on the textile guide

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,20 @@ module ApplicationHelper
   def redcloth(text)
     RedCloth.new(text).to_html.html_safe
   end
+  
+  # def render_side_menu
+  #   cat = Category.all
+  #   menu = "".html_safe
+  #   cat.each do |ctgry|
+  #     menu += content_tag(:span,ctgry.name)
+  #     articles = "".html_safe
+  #     arts = Article.find_by_category_id(ctgry.id)
+  #     arts.each do |art|
+  #       articles += content_tag(:li,art.name)
+  #     end
+  #     content_tag(:ul,articles)
+  #   end
+  #   menu
+  # end
+  
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -12,11 +12,14 @@ module ArticlesHelper
   
   def render_textile_design_guide
     list_elements = "".html_safe
-    list_elements += content_tag(:li,"* => enclose text between asterisks to get a bold text * bold text *")
-    list_elements += content_tag(:li,"_ => enclose text between underscores to get an Italic text * Italic text *")
-    list_elements += content_tag(:li,"- => enclose text between hypens to get a strikethrough text - strikethrough text -")
-    list_elements += content_tag(:li," + => enclose text between plus signs to get a underlined text * underlined text *")
+    list_elements += content_tag(:li,"* => enclose text between asterisks to get a bold text *bold text*")
+    list_elements += content_tag(:li,"_ => enclose text between underscores to get an Italic text  _Italic text_")
+    list_elements += content_tag(:li,"- => enclose text between hypens to get a strikethrough text -strikethrough text-")
+    list_elements += content_tag(:li," + => enclose text between plus signs to get a underlined text +underlined text+")
     list_elements += content_tag(:li," %{color:red} => enclose text between percentage signs and indicate the color between brackets to get a colored text %{color:red} red text %")
+    list_elements += content_tag(:li,"* bulleted list ")
+    list_elements += content_tag(:li,"** 2-level bulleted list ")
+
     content_tag(:ul, list_elements, :id => "textileGuide")
   end
   
