9e765a63c991debe8724268bde84aae91e3a3c98
ratuz <raul@innku.com>
2010-10-14 12:53:32 -0500

    admin can edit everything, users can edit own, fixed alerts not showing

--- user.rb
+++ user.rb
@@ -9,4 +9,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  def admin?
+    self.role == 'admin'
+  end
+  
 end
