c1086960c7ce3c2d75e0419126a14394b2181653
e3matheus <e3matheus@gmail.com>
2011-09-23 22:54:40 -0500

    merged with heroku

--- category.rb
+++
@@ -1,8 +0,0 @@
-class Category < ActiveRecord::Base
-  
-  has_many  :articles, :order => "name ASC"
-  has_many  :published, :conditions => {:published => true}, :class_name => "Article"
-  has_many  :drafts, :conditions => {:published => false}, :class_name => "Article"
-  
-  default_scope order("name ASC")
-end
