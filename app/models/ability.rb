27abc5dbbb36607f05e72757b5c9f8478063d17c
e3matheus <e3matheus@gmail.com>
2011-09-23 22:44:43 -0500

    refactored models

---
+++ app/models/ability.rb
@@ -0,0 +1,28 @@
+class Ability 
+  include CanCan::Ability
+    
+  def initialize(user)
+    alias_action :update, :destroy, :to => :modify
+    
+    user ||= User.new
+    
+    can :read, Article
+    
+    can :modify, Article do |article|
+      article.user == user
+    end
+    
+    if !user.new_record?
+      can :create, Article
+      can :manage, User do |u|
+        u == user
+      end
+    end
+    
+    if user.admin?
+      can :manage, Category
+      can :manage, Article
+    end
+    
+  end
+end
