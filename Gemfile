d08b0e30739b95035c40fdc0b54400a7eaaa658e
e3matheus <e3matheus@gmail.com>
2012-03-27 08:55:51 -0600

    [26733257] Como usuario puedo ver la fecha de la última modificación de un artículo
    https://www.pivotaltracker.com/story/show/26733257
    
    Lo único interesante a nivel técnico de este feature fue el uso de la gema de Timecop.
    La misma te permite controlar el tiempo en tus pruebas.

--- Gemfile
+++ Gemfile
@@ -25,4 +25,5 @@ group :test do
   gem 'cucumber-rails'
   gem 'database_cleaner'
   gem 'factory_girl_rails'
+  gem 'timecop'
 end
--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -12,6 +12,8 @@ Then /^I see the info for both articles$/ do
 end
 
 Given /^an article exists$/ do
+  new_time = Time.local(2012, 3, 27, 12, 0, 0)
+  Timecop.freeze(new_time)
   @article = FactoryGirl.create(:article)
 end
 
@@ -19,4 +21,6 @@ Given /^I see the info for the article$/ do
   page.should have_content("IE hate")
   page.should have_content("Las cosas que detestamos de IE")
   page.should have_content("Falta de convenciones")
+  page.should have_content("Actualizado el 27 de Mar, 2012")
+  Timecop.return
 end
